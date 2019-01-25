176935afd3d526e002250b8301ff7cb8e439f082
AnyMore <www.jihaichuan@qq.com>
2019-01-25 22:35:08 +0800

    haha

--- Scripts/pytest_000001.py
+++ Scripts/pytest_000001.py
@@ -1,4 +1,7 @@
 import allure
+import pytest
+
+
 class Test_001:
     @allure.step(title="我是测试步骤01")
     def test_000001_1(self):
@@ -9,3 +12,5 @@ class Test_001:
     def test_000001_2(self):
         print("------test_00000_2")
         assert False
+if __name__ == '__main__':
+    pytest.main(["pytest_000001.py"])
